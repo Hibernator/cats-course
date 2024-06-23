0b9e136f831194e8ead9d31c62f29f8223352ab3
Hibernator <gibernator@gmail.com>
2024-06-23 19:33:27 +0200

    Improve type classes

--- src/main/scala/part1recap/TypeClasses.scala
+++ src/main/scala/part1recap/TypeClasses.scala
@@ -9,13 +9,22 @@ object TypeClasses {
     def toJson(value: T): String
 
   // part 2 - create implicit type class INSTANCES
-  implicit object StringSerializer extends JSONSerializer[String]:
+//  implicit object StringSerializer extends JSONSerializer[String]:
+//    override def toJson(value: String): String = s"\"$value\""
+
+  given JSONSerializer[String] with
     override def toJson(value: String): String = s"\"$value\""
 
   implicit object IntSerializer extends JSONSerializer[Int]:
     override def toJson(value: Int): String = value.toString
 
-  implicit object PersonSerializer extends JSONSerializer[Person]:
+//  implicit object PersonSerializer extends JSONSerializer[Person]:
+//    override def toJson(value: Person): String =
+//      s"""
+//         |{ "name" : ${value.name}, "age" : ${value.age} }
+//         |""".stripMargin.trim
+
+  given JSONSerializer[Person] with
     override def toJson(value: Person): String =
       s"""
          |{ "name" : ${value.name}, "age" : ${value.age} }
