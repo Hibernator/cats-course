3d6bb90c1e3b1a8fd1110bcc2ab9e1f13b3d9ba8
Hibernator <gibernator@gmail.com>
2024-07-07 23:39:54 +0200

    Custom Monads

--- src/main/scala/part2abstractmath/CustomMonads.scala
+++ src/main/scala/part2abstractmath/CustomMonads.scala
@@ -60,15 +60,74 @@ object CustomMonads {
         case Branch(left, right) => Branch(stackRec(left), stackRec(right))
       }
 
-      case class Frame(mapped: List[Tree[B]], todos: List[Tree[Either[A, B]]])
+//      case class Frame(mapped: List[Tree[B]], todos: List[Tree[Either[A, B]]])
+//
+//      // https://stackoverflow.com/questions/55042834/how-to-make-tree-mapping-tail-recursive
+//      def tailRec(trees: List[Tree[Either[A, B]]]): Tree[B] = {
+//        trees match {
+//          case Leaf(Right(value)) :: tail =>
+//            tail match {
+//              case Nil => Leaf(value)
+//              case Leaf(Right(value)) :: more =>
+//              case Leaf(Left(value)) :: more =>
+//              case Branch(left, right) :: more =>
+//            }
+//          case Leaf(Left(value)) :: tail  =>
+//          case Branch(left, right) :: tail =>
+//        }
+//      }
 
-      // https://stackoverflow.com/questions/55042834/how-to-make-tree-mapping-tail-recursive
-      def tailRec(stack: List[Frame]): Tree[B] = ???
+//      tailRec(List(f(a)))
 
-      tailRec(List(Frame(Nil, List(f(a)))))
-      stackRec(f(a))
+      /*
+        assume that f(a) returns the following tree
+
+                _____1_____
+             __2__       __3__
+            /     \     /     \
+           L1     R2   R3     R4
+
+          B4 - value obtained from R4
+
+        tr([1], [], []) =
+        tr([3, 2, 1], [1], []) =
+        tr([R4, R3, 3, 2, 1], [1, 3], []) =
+        tr([R3, 3, 2, 1], [1, 3], [B4]) =
+        tr([3, 2, 1], [1, 3], [B3, B4]) =
+        tr([2, 1], [1, 3], [B34]) =
+        tr([R2, L1, 2, 1], [1, 2, 3], [B34]) =
+        tr([L1, 2, 1], [1, 2, 3], [B2, B34]) =
+        tr([R1, 2, 1], [1, 2, 3], [B2, B#$]) =
+        tr([2, 1], [1, 2, 3], [B1, B2, B34]) =
+        tr([1], [1, 2, 3], [B12, B34]) =
+        tr([], [1, 2, 3], [B1234]) =
+        B1234
+       */
+      def tailRec(todo: List[Tree[Either[A, B]]], expanded: Set[Tree[Either[A, B]]], done: List[Tree[B]]): Tree[B] =
+        if (todo.isEmpty) done.head
+        else
+          todo.head match {
+            case Leaf(Left(v))  => tailRec(f(v) :: todo.tail, expanded, done)
+            case Leaf(Right(b)) => tailRec(todo.tail, expanded, Leaf(b) :: done)
+            case node @ Branch(left, right) =>
+              if (!expanded.contains(node)) {
+                tailRec(right :: left :: todo, expanded + node, done)
+              } else {
+                val newLeft = done.head
+                val newRight = done.tail.head
+                val newBranch = Branch(newLeft, newRight)
+                tailRec(todo.tail, expanded, newBranch :: done.drop(2))
+              }
+          }
+
+      tailRec(List(f(a)), Set(), List())
+//      stackRec(f(a))
     }
   }
 
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = {
+    val tree: Tree[Int] = Branch(Leaf(10), Leaf(20))
+    val changedTree = treeMonad.flatMap(tree)(v => Branch(Leaf(v + 1), Leaf(v + 2)))
+    println(changedTree)
+  }
 }
